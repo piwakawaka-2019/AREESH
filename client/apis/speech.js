57c05fcad9a7eb056e499cd23de7863871b54fb5
Des Whelan <wheland6@tcd.ie>
2019-05-10 12:07:35 +1200

    check spelling

--- client/apis/speech.js
+++ client/apis/speech.js
@@ -6,3 +6,32 @@ export function transcribeSpeechAPI (fileName) {
     .send({fileName: fileName})
     .then(res => res.body['transcript'])
 }
+
+export function checkSpelling (word, spelling) {
+    let score = []
+
+    let spellingArr = spelling.split("").filter(char => {
+        return char != " "
+    })
+  
+    for (var i = 0; i < word.length; i++) {
+        if(spellingArr[i] == word.charAt(i)){
+            score.push(word.charAt(i))
+        } else {
+            score.push(spellingArr[i], 'X')
+            console.log("Incorrect: ", score)
+            return score
+        }
+    }
+
+    score.push('✓')
+    console.log("Winner: ", score)
+    return score
+}
+
+let word = 'sonic'
+let correctSpelling = "s o n i c"
+let incorrectSpelling = "s o m i c"
+
+checkSpelling(word, correctSpelling)
+checkSpelling(word, incorrectSpelling)
