c66f14d45f8b7a6552775e4a90af33f6b175bf8d
Joshgull23 <joshgulliver23@gmail.com>
2019-05-13 11:30:40 +1200

    updated instructions message

--- client/components/gameComponents/Home.jsx
+++ client/components/gameComponents/Home.jsx
@@ -15,7 +15,7 @@ class Home extends Component {
       <Fragment>
         <div className="card text-center my-5 ">
           <div className="card-body  ">
-            <h5 className="card-title">Instructions</h5>
+            <h5 className="card-title">INSTRUCTIONS</h5>
             <p className="card-text">
             AREESH! Fusing speech-recognition technology and your knowledge together to help you test your memory and sharpen your spelling skills! Let the app know the word you want to learn. It will briefly display on your screen before disappearing. Now over to you. Spell the word out loud, and AREESH will let you know if you got it right or wrong. Login/Register to keep a hold of your score and compete against other willing learners like yourself.
             </p>
