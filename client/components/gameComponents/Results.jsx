c7517e10e5041b0d58f616f81d24d66fc2b6d9f3
jesse collier <jesse.collier97@gmail.com>
2019-05-13 15:01:16 +1200

    added toLowerCase method

--- client/components/gameComponents/Results.jsx
+++ client/components/gameComponents/Results.jsx
@@ -1,104 +1,107 @@
-import React, { Component, Fragment as F} from 'react'
-import { connect } from 'react-redux'
+import React, { Component, Fragment as F } from "react";
+import { connect } from "react-redux";
 // are we using this library?
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-import {transcribeSpeech, checkSpelling} from '../../apis/speech'
-import {changeView, setWordCorrect} from '../../actions/game'
-import Winner from './Winner'
+import { transcribeSpeech, checkSpelling } from "../../apis/speech";
+import { changeView, setWordCorrect } from "../../actions/game";
+import Winner from "./Winner";
 
 export class Results extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      // for speech-recognition version 
+      // for speech-recognition version
       result: "",
       winnerDisplayed: false
-    }
+    };
 
-    this.handleClick = this.handleClick.bind(this)
-    this.updateResult = this.updateResult.bind(this)
-    this.displayWinner = this.displayWinner.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.updateResult = this.updateResult.bind(this);
+    this.displayWinner = this.displayWinner.bind(this);
   }
 
-  componentDidMount (e) {
-    let {word, spellingAttempt} = this.props
+  componentDidMount(e) {
+    let { word, spellingAttempt } = this.props;
 
-    let result = checkSpelling(word, spellingAttempt)
+    let result = checkSpelling(word, spellingAttempt);
 
     this.setState({
-      result: "",
-    })
+      result: ""
+    });
 
     result.forEach((letter, i) => {
-      setTimeout(() => this.updateResult(letter), 500 * i)
-    })
+      setTimeout(() => this.updateResult(letter), 500 * i);
+    });
   }
-  
-  // for speech-recognition version 
-  handleClick () {
-    transcribeSpeech('flacSonic.flac')
-    .then(transcription => checkSpelling("sonic", transcription))
-    .then(result => {
-      this.setState({
-        result: ""
-      })
-      result.forEach((letter, i) => {
-        setTimeout(() => this.updateResult(letter), 750 * i)
-      })
-    })
+
+  // for speech-recognition version
+  handleClick() {
+    transcribeSpeech("flacSonic.flac")
+      .then(transcription => checkSpelling("sonic", transcription))
+      .then(result => {
+        this.setState({
+          result: ""
+        });
+        result.forEach((letter, i) => {
+          setTimeout(() => this.updateResult(letter), 750 * i);
+        });
+      });
   }
-  
-  updateResult (letter) {
+
+  updateResult(letter) {
     this.setState({
       result: this.state.result + letter
-    })
+    });
 
     // display winner component if spellcheck complete
-    if(letter == 'X' || letter == '✓'){
+    if (letter == "X" || letter == "✓") {
       this.setState({
         winnerDisplayed: true
-      })
+      });
     }
 
-    let {word, spellingAttempt, dispatchWordCorrect} = this.props
+    let { word, spellingAttempt, dispatchWordCorrect } = this.props;
 
-    if(word == spellingAttempt){
-      dispatchWordCorrect(true)
-    } else dispatchWordCorrect(false)
+    if (word.toLowerCase() == spellingAttempt.toLowerCase()) {
+      dispatchWordCorrect(true);
+    } else dispatchWordCorrect(false);
   }
 
-  displayWinner () {
+  displayWinner() {
     this.setState({
       winnerDisplayed: true
-    })
+    });
   }
 
-  changeView = (e) => {
-    e.preventDefault()
-    this.props.displayWinner()
-  }
+  changeView = e => {
+    e.preventDefault();
+    this.props.displayWinner();
+  };
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <F>
         <h1>{this.state.result}</h1>
-        <Winner winnerDisplayed={this.state.winnerDisplayed}/>
+        <Winner winnerDisplayed={this.state.winnerDisplayed} />
       </F>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => ({
   word: state.game.wordData.word,
   spellingAttempt: state.game.wordData.spellingAttempt
-})
+});
 
 const mapDispatchToProps = dispatch => {
   return {
     displayWinner: e => dispatch(changeView("displayWinner")),
     dispatchWordCorrect: wordcorrect => dispatch(setWordCorrect(wordcorrect))
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Results)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Results);
