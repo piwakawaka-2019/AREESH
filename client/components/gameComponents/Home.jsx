96206816c2b4363413ecc065381aea372539a52a
Joshgull23 <joshgulliver23@gmail.com>
2019-05-13 12:10:17 +1200

    Instructions/Information improved + styles

--- client/components/gameComponents/Home.jsx
+++ client/components/gameComponents/Home.jsx
@@ -15,10 +15,14 @@ class Home extends Component {
       <Fragment>
         <div className="card text-center my-5 ">
           <div className="card-body  ">
-            <h5 className="card-title">INSTRUCTIONS</h5>
-            <p className="card-text">
-            AREESH! Fusing speech-recognition technology and your knowledge together to help you test your memory and sharpen your spelling skills! Let the app know the word you want to learn. It will briefly display on your screen before disappearing. Now over to you. Spell the word out loud, and AREESH will let you know if you got it right or wrong. Login/Register to keep a hold of your score and compete against other willing learners like yourself.
-            </p>
+            <h5 className="instructions">INSTRUCTIONS/INFORMATION</h5>
+            <ul className="instructions">
+            <li>Fusing speech-recognition technology and your knowledge together to help you test your memory and sharpen your spelling skills!</li> 
+            <li>Let the app know the word you want to learn</li>
+            <li>It will briefly display on your screen before disappearing</li>
+            <li>Now over to you, spell the word out loud and AREESH will let you know if you got it right or wrong</li>
+            <li>Login/Register to keep a hold of your score and compete against other willing learners like yourself</li>
+            </ul>
           </div>
         </div>
         <div className="d-flex justify-content-center">
