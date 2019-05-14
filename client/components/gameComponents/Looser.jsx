d4ad50df88daa1061fd60d35906a3c8ec576dc8c
runminty <zengzhenhuang@gmail.com>
2019-05-14 12:04:15 +1200

    sizing booms

--- client/components/gameComponents/Looser.jsx
+++ client/components/gameComponents/Looser.jsx
@@ -9,10 +9,10 @@ class Looser extends Component {
         return (
             <div className="sinir">
                 <div className="box" id="box1"><img src="/images/boom.png" width="100" height="100"  alt="Card image cap"></img> </div>
-                <div className="box" id="box2"><img src="/images/boom.png" width="100" height="100"  alt="Card image cap"></img></div>
-                <div className="box" id="box3"><img src="/images/boom.png" width="100" height="100" alt="Card image cap"></img></div>
-                <div className="box" id="box4"><img src="/images/boom.png" width="100" height="100" alt="Card image cap"></img></div>
-                <div className="box" id="box5"><img src="/images/boom.png" width="100" height="100" alt="Card image cap"></img></div>
+                <div className="box" id="box2"><img src="/images/boom.png" width="150" height="150"  alt="Card image cap"></img></div>
+                <div className="box" id="box3"><img src="/images/boom.png" width="150" height="150" alt="Card image cap"></img></div>
+                <div className="box" id="box4"><img src="/images/boom.png" width="50" height="50" alt="Card image cap"></img></div>
+                <div className="box" id="box5"><img src="/images/boom.png" width="75" height="75" alt="Card image cap"></img></div>
             </div>
         );
     }
