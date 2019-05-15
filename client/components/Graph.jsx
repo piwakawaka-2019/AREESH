0a2a77d099ba894edd86edcdfada39e21d53e854
Joshgull23 <joshgulliver23@gmail.com>
2019-05-15 13:21:52 +1200

    Dummy graph working

--- client/components/Graph.jsx
+++ client/components/Graph.jsx
@@ -1,55 +1,52 @@
-import React, { Component, Fragment as F } from 'react';
-import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
+import React, { Fragment as F } from 'react';
+import { VictoryAxis, VictoryLine, VictoryChart } from 'victory';
 
-
-const data = [
-    {quarter: 1, earnings: 13000},
-    {quarter: 2, earnings: 16500},
-    {quarter: 3, earnings: 14250},
-    {quarter: 4, earnings: 19000}
-  ];
-class Graph extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
-    }
-    render() {
-
-        
-        return (
-            
-            <F>
-                <div className="chart">
-                        <VictoryChart
-                    // domainPadding will add space to each side of VictoryBar to
-                    // prevent it from overlapping the axis
-                    domainPadding={20}
-                >
-                    <VictoryAxis
-                    // tickValues specifies both the number of ticks and where
-                    // they are placed on the axis
-                    tickValues={[1, 2, 3, 4]}
-                    tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
-                    />
-                    <VictoryAxis
-                    dependentAxis
-                    // tickFormat specifies how ticks should be displayed
-                    tickFormat={(x) => (`$${x / 1000}k`)}
-                    />
-                    <VictoryBar
-                    data={data}
-                    x="quarter"
-                    y="earnings"
-                    />
-                </VictoryChart>
- 
-                </div>
-
-            </F>
-            
+class Graph extends React.Component {
+  render() {
+    return (
+        <F>
+         <VictoryChart
+          domainPadding={{x: 40}}
+          style={{marginLeft: 120}}
+         >
+          <VictoryLine
+            data={[
+                {weeks: 1, wordslearnt: 10},
+                {weeks: 2, wordslearnt: 28},
+                {weeks: 3, wordslearnt: 30},
+                {weeks: 4, wordslearnt: 25},
+                {weeks: 5, wordslearnt: 35},
+                {weeks: 6, wordslearnt: 43},
+                {weeks: 7, wordslearnt: 32},
+                {weeks: 8, wordslearnt: 59},
+                 ]}
+            x="weeks"
+            y="wordslearnt"
+            style={{
+              data: {stroke: "#FFB12E", strokeWidth: 3},
+              labels: {fontSize: 14}
+            }}
+           />
+          <VictoryAxis
+            label="Weeks"
+            style={{
+              axisLabel: { padding: 30 }
+            }}
+          />
+          <VictoryAxis dependentAxis
+            label="Words Learnt"
+            style={{
+              axisLabel: { padding: 40 }
+            }}
+          />
+      </VictoryChart>
+    </F>
+   
         );
+      }
     }
-}
+            
+
 
 export default Graph;
 
