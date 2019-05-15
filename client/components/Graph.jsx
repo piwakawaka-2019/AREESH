c9f0eeb8d941ce5f937478b612dceee891388a5f
Des Whelan <wheland6@tcd.ie>
2019-05-15 17:16:26 +1200

    populate graph (wip)

--- client/components/Graph.jsx
+++ client/components/Graph.jsx
@@ -1,52 +1,87 @@
 import React, { Fragment as F } from 'react';
 import { VictoryAxis, VictoryLine, VictoryChart } from 'victory';
+import {connect} from 'react-redux'
+
+
 
 class Graph extends React.Component {
-  render() {
-    return (
-        <F>
-         <VictoryChart
-          domainPadding={{x: 40}}
-          style={{marginLeft: 120}}
-         >
-          <VictoryLine
-            data={[
-                {weeks: 1, wordslearnt: 10},
-                {weeks: 2, wordslearnt: 28},
-                {weeks: 3, wordslearnt: 30},
-                {weeks: 4, wordslearnt: 25},
-                {weeks: 5, wordslearnt: 35},
-                {weeks: 6, wordslearnt: 43},
-                {weeks: 7, wordslearnt: 32},
-                {weeks: 8, wordslearnt: 59},
-                 ]}
-            x="weeks"
-            y="wordslearnt"
-            style={{
-              data: {stroke: "#FFB12E", strokeWidth: 3},
-              labels: {fontSize: 14}
-            }}
-           />
-          <VictoryAxis
-            label="Weeks"
-            style={{
-              axisLabel: { padding: 30 }
-            }}
-          />
-          <VictoryAxis dependentAxis
-            label="Words Learnt"
-            style={{
-              axisLabel: { padding: 40 }
-            }}
-          />
-      </VictoryChart>
-    </F>
-   
-        );
-      }
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      lastSevenDays: []
     }
-            
+  }
+
+  getTheLastSevenDays (date) {
+    let lastSevenDays = []
+
+    for (var i = 0; i < 7; i++){
+        lastSevenDays.push(new Date(now.getFullYear(), now.getMonth(), now.getDate()-i))
+    }
+
+    this.setState({
+      lastSevenDays: lastSevenDays
+    })
+  }
+
+ render() {
+
+  console.log(this.state.lastSevenDays)
+  console.log(this.props.gameHistory)
+
+   return (
+       <F>
+        <VictoryChart
+         domainPadding={{x: 40}}
+         style={{marginLeft: 120}}
+        >
+         <VictoryLine
+           data={[
+               {days: "Mon", wordslearnt: 10},
+               {days: "Tue", wordslearnt: 28},
+               {days: "Wed", wordslearnt: 30},
+               {days: "Thu", wordslearnt: 25},
+               {days: "Fri", wordslearnt: 35},
+               {days: "Sat", wordslearnt: 43},
+               {days: "Sun", wordslearnt: 32},
+                ]}
+           x="days"
+           y="wordslearnt"
+           style={{
+             data: {stroke: "#FFB12E", strokeWidth: 3},
+             labels: {fontSize: 14}
+           }}
+          />
+         <VictoryAxis
+           label="Weeks"
+           style={{
+             axisLabel: { padding: 30 }
+           }}
+         />
+         <VictoryAxis dependentAxis
+           label="Words Learnt"
+           style={{
+             axisLabel: { padding: 40 }
+           }}
+         />
+     </VictoryChart>
+   </F>
+
+       );
+     }
+   }
 
 
-export default Graph;
+   const mapStateToProps = state => ({
+       gameHistory: state.userGameHistory
+     });
 
+     const mapDispatchToProps = dispatch => {
+       return 1
+     };
+
+     export default connect(
+       mapStateToProps,
+       mapDispatchToProps
+     )(Graph);
