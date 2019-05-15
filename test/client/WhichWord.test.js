2bf55defb342810974c7a944bb3c17638c1d8d21
jesse collier <jesse.collier97@gmail.com>
2019-05-15 16:03:23 +1200

    test issue fixed with ross

---
+++ test/client/WhichWord.test.js
@@ -0,0 +1,5 @@
+import WhichWord from "../../client/components/gameComponents/WhichWord";
+import { exportAllDeclaration } from "@babel/types";
+test("test", () => {
+  expect(1).toEqual(1);
+});
---
+++ test/jest.init.js
@@ -0,0 +1,5 @@
+console.log("jest init");
+
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import "babel-polyfill";
